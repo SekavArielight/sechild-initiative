d564983271d5c3dfe4697d9b9243becc412f091d
God'swill Sekav Apya <godswillapya@gmail.com>
2025-03-08 23:34:56 +0100

    add Navbar, Hero components; remove unused assets; create Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({title}) => {
   return (
-    <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">{title}</button>
+    <button className="bg-blue-600 text-white px-4 py-2 rounded-[10px] hover:bg-blue-700 text-sm">{title}</button>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import Button from './Button'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex items-center justify-between px-32 py-5">
         <a href="/"><img src="/src/assets/hilink-logo.svg" alt="logo" /></a>
 
         <div>
             {navigation.map((item) => (
-                <a href={item.url} key={item.id}>
+                <a href={item.url} key={item.id} className="mx-3 text-sm">
                     {item.title}
                 </a>
             ))}
