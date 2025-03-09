52b211247f6c4a7553631f796c8b6fe16ad5c1ab
God'swill Sekav Apya <godswillapya@gmail.com>
2025-03-09 04:44:43 +0100

    update Navbar and Hero components with new styles and buttons; enhance layout and accessibility

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header
+      className="relative flex flex-col items-center justify-center h-screen text-center sm:px-32 sm:py-5 max-w-screen-2xl mx-auto"
+      style={{
+        backgroundImage: "url('/src/assets/images/hero-image-1.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <h1 className="sm:text-7xl text-[#fff] z-50">Empowering Every Child, Embracing Every Ability</h1>
+      <h3 className="sm:text-[17px] text-[#fff]/80 z-50 my-12">
+        At SECHILD, we are committed to creating a world where every child with
+        special needs has access to education, healthcare, and equal
+        opportunities. Together, we can make a difference!
+      </h3>
 
-export default Hero
+      <div>
+        <Button title="Find Out More"/>
+        <Button title="Donate Now"/>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import Button from './Button'
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-32 py-5">
+    <nav className="bg-transparent absolute top-0 left-0 w-full z-50 flex items-center justify-between px-32 py-5">
         <a href="/"><img src="/src/assets/hilink-logo.svg" alt="logo" /></a>
 
         <div>
             {navigation.map((item) => (
-                <a href={item.url} key={item.id} className="mx-3 text-sm">
+                <a href={item.url} key={item.id} className="mx-3 text-sm text-[#fff]">
                     {item.title}
                 </a>
             ))}
