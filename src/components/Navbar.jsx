3b149c9b7c280f4ae16cac68a64aaea11df7c584
God'swill Sekav Apya <godswillapya@gmail.com>
2025-03-10 18:41:34 +0100

    add react-router dependency and update Navbar component to use Link for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { navigation } from '../constants'
-import Button from './Button'
+import React from "react";
+import { navigation } from "../constants";
+import Button from "./Button";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
     <nav className="bg-transparent absolute top-0 left-0 w-full z-50 flex items-center justify-between px-32 py-5">
-        <a href="/"><img src="/src/assets/hilink-logo.svg" alt="logo" /></a>
+      <a href="/">
+        <img src="/src/assets/hilink-logo.svg" alt="logo" />
+      </a>
 
-        <div>
-            {navigation.map((item) => (
-                <a href={item.url} key={item.id} className="mx-3 text-sm text-[#fff]">
-                    {item.title}
-                </a>
-            ))}
-        </div>
+      <div>
+        {navigation.map((item) => (
+          <Link
+            href={item.url}
+            key={item.id}
+            className="mx-3 text-sm text-[#fff]"
+          >
+            {item.title}
+          </Link>
+        ))}
+      </div>
 
-        <Button title="Donate"></Button>
+      <Button title="Donate"></Button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
